4bd0412770671fd47258a7f74aee9199d0cacc08
e-bondarev <82723220+e-bondarev@users.noreply.github.com>
2021-08-17 20:14:20 +0200

    Window & GUI abstraction

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,38 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-#include <imgui/imgui.h>
-#include <imgui/imgui_impl_glfw.h>
-#include <imgui/imgui_impl_opengl3.h>
+#include "core/window/window.h"
 
 int main(int argc, char* argv[])
 {
-	glfwInit();
-	glfwDefaultWindowHints();
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello, world!", nullptr, nullptr);
-	glfwMakeContextCurrent(window);
+	Win** window_pp = global::get_window_pp();
+	*window_pp = new Win();
+	Win& window = **window_pp;
 
-	glewInit();
+	global::gui::init();
 
-
-
-
-
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-    //io.ConfigViewportsNoAutoMerge = true;
-    //io.ConfigViewportsNoTaskBarIcon = true;
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
-
-    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
-    ImGuiStyle& style = ImGui::GetStyle();
-    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-    {
-        style.WindowRounding = 0.0f;
-        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-    }
-
-    // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init();
-
-
-
-	while (!glfwWindowShouldClose(window))
+	while (window.is_running())
 	{
-		glfwPollEvents();
+		window.poll_events();
+		
+		global::gui::begin_frame();
 
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
+			ImGui::ShowDemoWindow();		
 
-		ImGui::ShowDemoWindow();		
+			ImGui::Render();
 
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    	
-        // Update and Render additional Platform Windows
-        // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
-        //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
-        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-        {
-            GLFWwindow* backup_current_context = glfwGetCurrentContext();
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
-            glfwMakeContextCurrent(backup_current_context);
-        }
+			int display_w, display_h;
+			glfwGetFramebufferSize(window.get_handle(), &display_w, &display_h);
+			glViewport(0, 0, display_w, display_h);
+			glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		
+		global::gui::end_frame();
+
+		window.swap_buffers();
 	}
 
-	glfwTerminate();
+	global::gui::shutdown();
+
+	delete *global::get_window_pp();
 
 	return 0;
 }
