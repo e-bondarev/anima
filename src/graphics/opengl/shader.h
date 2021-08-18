3622e13ab72d9e8b2f9601aa90cf4a092f7349a1
e-bondarev <82723220+e-bondarev@users.noreply.github.com>
2021-08-18 10:06:41 +0200

    Drawing a quad

---
+++ src/graphics/opengl/shader.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "object.h"
+
+#include <string>
+
+class Shader : public GPUObject
+{
+public:
+	Shader(const std::string& vs_code, const std::string& fs_code);
+	~Shader();
+
+	void bind() override;
+	void unbind() override;
+
+private:
+	GLuint create_shader(const std::string code, GLuint shader_type);
+	void link() const;
+
+	GLuint vs_handle;
+	GLuint fs_handle;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,34 @@
 #include "core/window/window.h"
 
+#include "graphics/opengl/shader.h"
+#include "graphics/opengl/vao.h"
+
+const char* vs_code = R""""(
+
+#version 440 core
+
+layout (location = 0) in vec2 in_Position;
+
+void main()
+{
+	gl_Position = vec4(in_Position, 0.0, 1.0);
+}
+
+)"""";
+
+const char* fs_code = R""""(
+
+#version 440 core
+
+out vec4 out_Color;
+
+void main()
+{
+	out_Color = vec4(0.0, 0.0, 1.0, 1.0);
+}
+
+)"""";
+
 int main(int argc, char* argv[])
 {
 	Win** window_pp = global::get_window_pp();
@@ -7,6 +36,17 @@ int main(int argc, char* argv[])
 	Win& window = **window_pp;
 
 	global::gui::init();
+
+	Shader shader(vs_code, fs_code);
+
+	std::vector<Vertex> vertices = {
+		{ { -0.5f, -0.5f } },
+		{ {  0.5f, -0.5f } },
+		{ {  0.5f,  0.5f } },
+		{ { -0.5f,  0.5f } }
+	};
+
+	VAO vao(vertices, Vertex::GetLayout(), { 0, 1, 2, 2, 3, 0 });
 
 	while (window.is_running())
 	{
@@ -22,6 +62,14 @@ int main(int argc, char* argv[])
 			glfwGetFramebufferSize(window.get_handle(), &display_w, &display_h);
 			glViewport(0, 0, display_w, display_h);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			shader.bind();
+
+				vao.bind();
+					glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+				vao.unbind();
+
+			shader.unbind();
 
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		
